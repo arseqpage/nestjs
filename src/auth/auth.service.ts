71659c41ce9edb158e5500ab58b67289084f8e54
Арслан Жазыкпаев <arseqpage@gmail.com>
2026-01-10 17:24:05 +0300

    14 jwt

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,115 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterRequest } from './dto/register.dto';
+import { hash, verify } from 'argon2';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt.interface';
+import { LoginRequest } from './dto/login.dto';
+import { User } from '@prisma/client';
+import { Response } from 'express';
+import { isDev } from '../utils/is-dev.util';
+
+@Injectable()
+export class AuthService {
+  private readonly JWT_ACCESS_TOKEN_TTL;
+  private readonly JWT_REFRESH_TOKEN_TTL;
+
+  private readonly COOKIE_DOMAIN;
+
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {
+    this.JWT_ACCESS_TOKEN_TTL = configService.getOrThrow<string>(
+      'JWT_ACCESS_TOKEN_TTL',
+    );
+    this.JWT_REFRESH_TOKEN_TTL = configService.getOrThrow<string>(
+      'JWT_REFRESH_TOKEN_TTL',
+    );
+    this.COOKIE_DOMAIN = configService.getOrThrow<string>('COOKIE_DOMAIN');
+  }
+
+  async register(res: Response, request: RegisterRequest) {
+    const { name, email, password } = request;
+    const isUserExists = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (isUserExists) {
+      throw new ConflictException('User already exists');
+    }
+
+    const user = await this.prismaService.user.create({
+      data: {
+        name,
+        email,
+        password: await hash(password),
+      },
+    });
+
+    return this.auth(res, user.id);
+  }
+
+  async login(res: Response, dto: LoginRequest) {
+    const { email, password } = dto;
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const isPasswordValid = await verify(user.password, password);
+
+    if (!isPasswordValid) {
+      throw new NotFoundException('User not found');
+    }
+
+    return this.auth(res, user.id);
+  }
+
+  private auth(res: Response, id: string) {
+    const { accessToken, refreshToken } = this.generateTokens(id);
+
+    this.setCookie(res, refreshToken, new Date(60 * 60 * 24 * 7));
+
+    return { accessToken };
+  }
+
+  private generateTokens(id: string) {
+    const payload: JwtPayload = { id };
+    const accessToken = this.jwtService.sign(payload, {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      expiresIn: this.JWT_ACCESS_TOKEN_TTL,
+    });
+    const refreshToken = this.jwtService.sign(payload, {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      expiresIn: this.JWT_REFRESH_TOKEN_TTL,
+    });
+
+    return { accessToken, refreshToken };
+  }
+
+  private setCookie(res: Response, token: string, expires: Date) {
+    res.cookie('refreshToken', token, {
+      httpOnly: true,
+      expires,
+      domain: this.COOKIE_DOMAIN as string,
+      secure: !isDev(this.configService),
+      sameSite: isDev(this.configService) ? 'none' : 'lax',
+    });
+  }
+}
